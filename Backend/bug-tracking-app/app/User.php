a57f7f2ecf543a907e9c2116f1a38c07bcfabcc5
Ahmed Ossama <58595131+AhmedOssama22dev@users.noreply.github.com>
2022-05-04 16:23:39 +0200

    add user authentication

--- Backend/bug-tracking-app/app/User.php
+++ Backend/bug-tracking-app/app/User.php
@@ -5,7 +5,7 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Laravel\Passport\HasApiTokens;
 
 
 class User extends Authenticatable
@@ -18,7 +18,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'role_id', 'password'
     ];
 
     /**
--- Backend/bug-tracking-app/database/migrations/2014_10_12_000000_create_users_table.php
+++ Backend/bug-tracking-app/database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,8 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->foreignId('role_id')->constrained('roles');
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id')->references('id')->on('roles');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
