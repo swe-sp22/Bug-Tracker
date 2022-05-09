fe546b8baeba2f1aa0d1ac4c5946e096c66534e7
Ahmed Ossama <58595131+AhmedOssama22dev@users.noreply.github.com>
2022-05-09 15:04:04 +0200

    add-some-relations

--- Backend/bug-tracking-app/app/Bug.php
+++ Backend/bug-tracking-app/app/Bug.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Bug extends Model
 {
-    //
+    protected $fillable = ['title', 'description', 'photo', 'type'];
+    
+    public function project(){
+        return $this->belongsTo(Project::class);
+    }
+    
+    public function reporter(){
+        return $this->hasMany(User::class);
+    }
+    public function assignees(){
+        return $this->belongsTo(User::class);
+    }
 }
